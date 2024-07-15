c7f7a937942755b101a12ab2bdb2f1beea2f9721
Kil0-ui <jamesca12@yahoo.com>
2024-07-14 20:47:49 -0500

    Added basic fields

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,24 @@
 "use client"
 
-import { Button } from "@fluentui/react-components";
+import { Button, Field, SpinButton } from "@fluentui/react-components";
 import { pageWrapper, verticalStack } from "./styles";
 
 export default function Home() {
 
     return (
         <div className={`${verticalStack().root} ${pageWrapper().root}`}>
+            <Field label="Pregnancies">
+                <SpinButton min={0} max={30} />
+            </Field>
+            <Field label="Glucose Level">
+                <SpinButton min={0} max={500} />
+            </Field>
+            <Field label="BMI (Body Mass Index)">
+                <SpinButton precision={1} min={0} max={500} />
+            </Field>
+            <Field label="Age">
+                <SpinButton min={0} max={150} />
+            </Field>
             <div>
                 <Button appearance="primary">Predict</Button>
             </div>
