0b64612a1eb5facfeaaa594dd547c292d361d02a
Kil0-ui <jamesca12@yahoo.com>
2024-07-14 20:00:31 -0500

    Added appdir directive for fluent ui

--- src/app/Providers.tsx
+++ src/app/Providers.tsx
@@ -2,11 +2,10 @@
 
 import { RendererProvider, createDOMRenderer, SSRProvider, FluentProvider, webLightTheme, webDarkTheme } from "@fluentui/react-components";
 import React, { useState, useEffect } from "react";
-import { ThemeMode } from "./LayoutWrapper";
 
 const renderer = createDOMRenderer();
 
-export function Providers({ children, theme }: { children: React.ReactNode, theme: ThemeMode }) {
+export function Providers({ children, theme }: { children: React.ReactNode, theme: "light" | "dark" }) {
     // Declare a state variable named 'hasMounted' and a function named 'setHasMounted' to update it.
     const [hasMounted, setHasMounted] = useState(false);
 
